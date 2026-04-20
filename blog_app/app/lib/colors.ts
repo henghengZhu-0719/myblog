b187bb4b82229d9d30c1b9d774d63ebf00ecc868
henghengZhu-0719 <ma94086@163.com>
2026-04-20 16:35:17 +0800

    修复一些无关软件，并回档

--- blog_app/app/lib/colors.ts
+++
@@ -1,6 +0,0 @@
-export const PRIMARY = '#fe2c55';
-export const BG = '#f5f5f5';
-export const WHITE = '#fff';
-export const TEXT = '#333';
-export const GRAY = '#999';
-export const BORDER = '#eee';
--- blog_app/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-
-const projectRoot = __dirname;
-const config = getDefaultConfig(projectRoot);
-
-config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
-
-module.exports = config;
